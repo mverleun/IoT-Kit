002886d356eeca84b9a93349191aa1807ec23af2
mverleun <marco@marcoach.nl>
2018-06-27 08:45:02 +0200

    Started to work on VCC reporting

--- Sensor-BH1750/src/WemosD1BH1750.ino
+++ Sensor-BH1750/src/WemosD1BH1750.ino
@@ -13,6 +13,8 @@ StaticJsonBuffer<100> jsonBuffer;
 #include <Wire.h>
 #include <BH1750.h>
 
+ADC_MODE(ADC_VCC);
+
 // SCL = D1
 // SDA = D2
 #define MQTT_SERVER_FINGERPRINT {0xd6, 0x27, 0x18, 0xdd, 0x09, 0xce, 0x3d, 0x80, 0x1f, 0x59, 0x7a, 0x2b, 0x29, 0x78, 0x93, 0xe8, 0x0b, 0x82, 0x8a, 0x5d}
@@ -27,6 +29,7 @@ unsigned long minInterval = 1 * 60;   // Minimum interval between status changes
 unsigned long previousUpdate = 0L;
 
 HomieNode lightNode("light", "light");
+HomieNode vccNode("powerSupply", "voltage");
 
 void setupHandler() {
   lightNode.setProperty("unit").send("lux");
